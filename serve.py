e0d7fb8a9bdf26bcf965cf436abaf70a2da73db4
wasserth <j.wasserthal@dkfz.de>
2020-10-21 10:35:23 +0200

    Add browsing of weight files

--- serve.py
+++ serve.py
@@ -1,3 +1,5 @@
+import glob
+
 import numpy as np
 from flask import Flask, request, jsonify, send_file, send_from_directory
 from markupsafe import escape
@@ -6,23 +8,37 @@ from utils import is_valid_access_code
 
 app = Flask(__name__)
 
+def get_available_models():
+    available_models = [f for f in glob.glob("models/*.hdf5")]
+    return [m.split("/")[1].split(".")[0] for m in available_models]
 
 @app.route('/get_model', methods=["POST"])
 def get_model():
-    if is_valid_access_code(request.json["access_code"]):
-        model = "models/weights_coscia.hdf5"
-        return send_file(model), 200
+    meta = request.json
+    if is_valid_access_code(meta["access_code"]):
+        model = meta["name"]
+        if model in get_available_models():
+            model = "models/" + model + ".hdf5"  # todo: read and write access only to models dir
+            return send_file(model), 200
+        else:
+            return {"message": "invalid model name - not found"}, 500
     else:
         return {"message": "invalid access code"}, 401
 
 
 @app.route('/upload_model', methods=['POST'])
 def upload_file():
-    f = request.files['upload_file']
-    f.save('models/uploaded_file.hdf5')
-    content = request.json
-    print(content)
-    return {"message": "upload successfull"}, 200
+    meta = request.form.to_dict()
+    if is_valid_access_code(meta["access_code"]):
+        model = meta["name"]
+        if model in get_available_models():
+            f = request.files['upload_file']
+            f.save("models/" + model + ".hdf5")
+            return {"message": "upload successful"}, 200
+        else:
+            return {"message": "invalid model name - not found"}, 500
+    else:
+        return {"message": "invalid access code"}, 401
 
 
 if __name__ == '__main__':
